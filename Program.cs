40bb04480444e705510a1788cc0d9a80be6ab7bb
Stanley <stanley.alv3s@gmail.com>
2018-06-06 17:24:53 -0300

    MVC

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,30 +23,4 @@ namespace ProductCatalog
                 .Build();
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace ProductCatalog
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
-}
->>>>>>> e0468daa8a8c85f95ee414987f2e7741e8625989
